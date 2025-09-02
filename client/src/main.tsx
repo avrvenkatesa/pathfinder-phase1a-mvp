cdc9e300a8a0005b62180ee28e9695d3306880bb
avrvenkatesa <avr.venkatesa@gmail.com>
2025-09-02 00:51:30 +0000

    chore(x-tab): bootstrap cross-tab handlers before app render

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,10 @@
+// client/src/main.tsx
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { registerWorkflowCrossTabHandlers } from "./workflow/bootstrapCrossTab";
+
+// Register cross-tab handlers BEFORE rendering so we don't miss early events
+registerWorkflowCrossTabHandlers();
 
 createRoot(document.getElementById("root")!).render(<App />);
